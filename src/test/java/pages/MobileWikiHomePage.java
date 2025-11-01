2b4fe7b0928b5a8ae22770bb4514dbf8026237b3
harigithub1 <m.haribabu26@gmail.com>
2025-11-01 13:15:57 +0530

    enhancement

--- src/test/java/pages/MobileWikiHomePage.java
+++ src/test/java/pages/MobileWikiHomePage.java
@@ -5,49 +5,60 @@ import lombok.SneakyThrows;
 import org.openqa.selenium.By;
 
 public class MobileWikiHomePage extends BaseScreenMobile {
-  public MobileWikiHomePage(AppiumDriver driver) {
-    super(driver);
-  }
+    public MobileWikiHomePage(AppiumDriver driver) {
+        super(driver);
+    }
 
-  /**
-   * Mobile Elements
-   */
-  private final By OKAndroid = By.xpath("//*[@name='OK' or @text='OK']");
-  private final By OKIOS = By.xpath("//*[@name='username' or @label='username']");
-  private By Ok() {
-    return isAndroid() ? OKAndroid : OKIOS;
-  }
+    /**
+     * Mobile Elements
+     */
+    private final By OKAndroid = By.xpath("//*[@name='OK' or @text='OK']");
+    private final By OKIOS = By.xpath("//*[@name='username' or @label='username']");
 
-  private final By ListByAndroid = By.xpath("(//*[@resource-id='org.wikipedia.alpha:id/icon'])[2]");
-  private final By ListByIOS = By.xpath("//*[@name='username' or @label='username']");
-  private By ListBy() {
-    return isAndroid() ? ListByAndroid : ListByIOS;
-  }
+    private By Ok() {
+      return isAndroid() ? OKAndroid : OKIOS;
+    }
 
-  private final By HistoryOrOkAndroid = By.xpath("//*[@resource-id='com.example:id/password' or @content-desc='password']");
-  private final By HistoryOrOkIOS = By.xpath("//*[@name='password' or @label='password']");
-  private By HistoryOrOk() {
-    return isAndroid() ? HistoryOrOkAndroid : HistoryOrOkIOS;
-  }
+    private final By allowAndroid = By.xpath("//*[@name='Allow' or @text='Allow']");
+    private final By allowIOS = By.xpath("//*[@name='username' or @label='username']");
+
+    private By allow() {
+        return isAndroid() ? allowAndroid : allowIOS;
+    }
+
+    private final By ListByAndroid = By.xpath("(//*[@resource-id='org.wikipedia.alpha:id/icon'])[2]");
+    private final By ListByIOS = By.xpath("//*[@name='username' or @label='username']");
+
+    private By ListBy() {
+        return isAndroid() ? ListByAndroid : ListByIOS;
+    }
+
+    private final By HistoryOrOkAndroid = By.xpath("//*[@resource-id='com.example:id/password' or @content-desc='password']");
+    private final By HistoryOrOkIOS = By.xpath("//*[@name='password' or @label='password']");
+
+    private By HistoryOrOk() {
+        return isAndroid() ? HistoryOrOkAndroid : HistoryOrOkIOS;
+    }
 
 
-  /**
-   * Actions
-   */
-  @SneakyThrows
-  public void userOnWikiHomePage() {
-    waitAndClick(Ok());
-  }
+    /**
+     * Actions
+     */
+    @SneakyThrows
+    public void userOnWikiHomePage() {
+        waitAndClick(Ok());
+        waitAndClick(allow());
+    }
 
-  @SneakyThrows
-  public void clickListElement() {
-    waitAndClick(ListBy());
+    @SneakyThrows
+    public void clickListElement() {
+        waitAndClick(ListBy());
 //    test.get().log(Status.INFO, "Clicked List/Alert", MediaEntityBuilder.createScreenCaptureFromBase64String(takeScreenshotAsBase64()).build());
-  }
+    }
 
-  @SneakyThrows
-  public void clickHistoryElement() {
-    waitAndClick(HistoryOrOk());
+    @SneakyThrows
+    public void clickHistoryElement() {
+        waitAndClick(HistoryOrOk());
 //    test.get().log(Status.INFO, "Clicked History/OK", MediaEntityBuilder.createScreenCaptureFromBase64String(takeScreenshotAsBase64()).build());
-  }
+    }
 }
